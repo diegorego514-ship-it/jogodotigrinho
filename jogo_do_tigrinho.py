eb2df7a8b535302a5a064eb9b45fb81211c8557b
Diego Rêgo <diegorego514@gmail.com>
2026-03-12 19:58:50 -0300

    Enhance game with additional guessing options
    Added a second guessing loop for user choices from 1 to 10.

--- jogo_do_tigrinho.py
+++ jogo_do_tigrinho.py
@@ -1,13 +1,13 @@
 import random 
 noedas_acerto = 10000000
 moedas_ganhas = 90
-
-
+numeros_de_escolhas = 10
+numeros_de_tentativas = 15
 
 jogotigre = input("Digite seu nome para começarmos: ");
 
 while True:
-    chute = input("quantas rodadas de 1 a 3: ");
+    chute = input("Quantas rodadas de 1 a 3: ");
     correto = random.randint(1, 3);
 
     if chute.isdigit() and int(chute) == correto:
@@ -19,3 +19,16 @@ while True:
         if tente != "S":
             print("Jogo encerrado");
             break
+
+while True:
+    escolhas = input("Quantas escolhas de 1 a 10: ");
+    correto = random.randint(1 , 3);
+
+    if escolhas.isdigit() and int(escolhas) == correto:
+        print("Você acertou")
+        break
+    else:
+        tente = input("Quer tentar de novo S/N: ").strip()
+
+        if tente != "S":
+            print("Jogo encerrado");
